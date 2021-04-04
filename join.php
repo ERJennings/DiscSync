a83c6d767d05fd2d2a304429dcb5fe99acc3f784
ERJennings <erjennings13@gmail.com>
2021-04-04 18:25:18 -0400

    Working on creating scoresheet table

--- join.php
+++ join.php
@@ -10,20 +10,20 @@
 
 <?php
 
-$conn = new mysqli($_SERVER['discsync2.cyudrahusm5z.us-east-1.rds.amazonaws.com'],
-    $_SERVER['admin'], $_SERVER['365DaOfAmTr'], $_SERVER['discsyncdb'], $_SERVER['3306']);
+$conn = new mysqli('discsync2.cyudrahusm5z.us-east-1.rds.amazonaws.com',
+    'admin', '365DaOfAmTr', 'discsyncdb', '3306');
 
 echo "<html><body>
 
 <form join=\"join.php\" method=\"post\">
 
-    <input class = \"input\", type=\"number\" name=\"id\">
+    <input class = \"input\" type=\"number\" name=\"id\">
 
     <br> <p>
 
     <button class = \"button\" type=\"submit\" name=\"submit\"><i>Start</i></form>";
 
-$num = $_POST['id'];
+$gameID = $_POST['id'];
 
 ?>
 
--- scoresheet.php
+++ scoresheet.php
@@ -7,19 +7,55 @@
 <body style ="background-color:rgb(63, 192, 235);">
 
 <?php
-$conn = new mysqli($_SERVER['discsync2.cyudrahusm5z.us-east-1.rds.amazonaws.com'],
-    $_SERVER['admin'], $_SERVER['365DaOfAmTr'], $_SERVER['discsyncdb'], $_SERVER['3306']);
+$conn = new mysqli('discsync2.cyudrahusm5z.us-east-1.rds.amazonaws.com',
+    'admin', '365DaOfAmTr', 'discsyncdb', '3306');
 
-$rows = 19;
-$columns = 4;
+//$gameID = 1;
+$numPlayers = 2;
+
+echo "<html>
+
+<body>
+
+<form scoresheet=\"scoresheet.php\" method=\"post\">
+    
+    <table class = \"table\">
+    <!--Headings-->
+    <tr class = \"tr\">
+        <th>Hole</th>
+        <th>Par</th>
+        <th><input class = \"cell\" type=\"text\" name=\"p1\" value=\"P1\"></th>
+        <th><input class = \"cell\" type=\"text\" name=\"p2\" value=\"P2\"></th>
+        <th><input class = \"cell\" type=\"text\" name=\"p3\" value=\"P3\"></th>
+        <th><input class = \"cell\" type=\"text\" name=\"p4\" value=\"P4\"></th>
+        <th><input class = \"cell\" type=\"text\" name=\"p5\" value=\"P5\"></th>
+        <th><input class = \"cell\" type=\"text\" name=\"p6\" value=\"P6\"></th>
+    </tr>
+    <!--Hole 1-->
+    <tr class = \"tr\">
+        <td>1</td>
+        <td><input class = \"cell\" type=\"number\" name=\"par1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p1h1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p2h1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p3h1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p4h1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p5h1\"></td>
+        <td><input class = \"cell\" type=\"number\" name=\"p6h1\"></td>
+    </tr>
+</table>
+
+    <br> <p>
+
+    <button class = \"button\" type=\"submit\" name=\"submit\"><i>Update</i></form>";
+
 ?>
-
+<!--
 <table class = "table">
     <tr class = "tr">
         <th>Hole</th>
         <th>Par</th>
-        <th>Eric</th>
-        <th>John</th>
+        <th>P1</th>
+        <th>P2</th>
     </tr>
     <tr class = "tr">
         <td>1</td>
@@ -34,6 +70,6 @@ $columns = 4;
         <td>4</td>
     </tr>
 </table>
-
+-->
 </body>
 </html>
